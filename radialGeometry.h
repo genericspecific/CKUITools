84ae2f892f2e98f4a203266d6d1adebf49b7e73a
Constantine Karlis <dino@constantinekarlis.com>
2013-02-10 21:05:15 +0100

    Added an import to stop breakage

--- radialGeometry.h
+++ radialGeometry.h
@@ -12,6 +12,9 @@
 // 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#ifndef radialGeometry_h
+#define radialGeometry_h
+
 typedef struct {
     float radius;
     float angle;
@@ -22,3 +25,5 @@ float radiansToDegrees(float radians);
 radialPoint radialPointMake(float radius , float angle);
 CGPoint polarToCartesian(radialPoint point);
 radialPoint cartesianToPolar(CGPoint point);
+
+#endif
